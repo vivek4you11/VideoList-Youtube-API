e395439b25492f729be420367286cbfeceeb98a5
Vivek Mehta <vivekisback111@gmail.com>
2019-01-02 07:33:31 +0530

    First Commit

---
+++ src/apis/youTube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const KEY = 'AIzaSyBAPmVeuE0ubRTfmY9LnRSlvxlHB95ivL4';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import youTube from '../apis/youTube';
+import VideoList from './VideoList';
+import VideoDetails from './VideoDetails';
+
+class App extends Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
+  onTermSubmit = async term => {
+    const response = await youTube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({
+      selectedVideo: video
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetails video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
